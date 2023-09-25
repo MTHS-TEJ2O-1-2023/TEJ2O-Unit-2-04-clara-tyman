3398734504b40c6eb7b895c7e1e3c1c3643f2bc7
Clara <144264170+clara-tyman@users.noreply.github.com>
2023-09-25 16:12:17 +0000

    python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,18 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Clara Tyman 
+Created on: Sep 2023
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+display.clear()
+display.show(Image.HAPPY)
+
+temp = 0
+
+while True:
+    if button_a.is_pressed():
+        temp = temperature()
+        display.scroll("The temperature is" + str(temp) + " C.")
